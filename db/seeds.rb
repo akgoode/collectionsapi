1ce6c24caf24ff2782c6793389be6b4a54325f16
akgoode <akgoode@gmail.com>
2017-07-30 17:58:33 -0500

    made seeds file have a bunch of objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,102 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.create([
+  {
+    name: "Chair",
+    description: "Comfy Chair",
+    category: "furniture",
+    creator: "Kenzie"
+  },
+  {
+    name: "Necklace",
+    description: "Beautiful Silver Necklace",
+    category: "jewelry",
+    creator: "Betsy Rosen"
+  },
+  {
+    name: "Table",
+    description: "Brown Table",
+    category: "furniture",
+    creator: "Andrew Goode"
+  },
+  {
+    name: "TV",
+    description: "55in. TV",
+    category: "furniture",
+    creator: "Panasonic"
+  },
+  {
+    name: "Silver Coin",
+    description: "An old silver coin",
+    category: "silver",
+    creator: "Augustus Caesar"
+  },
+  {
+    name: "Black Purse",
+    description: "A black purse made out of leather",
+    category: "purses",
+    creator: "Prada"
+  },
+  {
+    name: "Spoon",
+    description: "A spoon made out of jade",
+    category: "objects",
+    creator: "Someone on the Titanic"
+  },
+  {
+    name: "Picture Frame",
+    description: "A picture frame from the 1980's",
+    category: "objects",
+    creator: "Kodak"
+  },
+  {
+    name: "Purple Chair",
+    description: "Comfy Chair",
+    category: "furniture",
+    creator: "Kenzie"
+  },
+  {
+    name: "Bracelet",
+    description: "Beautiful Gold Bracelet",
+    category: "jewelry",
+    creator: "Betsy Rosen"
+  },
+  {
+    name: "Footstool",
+    description: "Wooden footstool",
+    category: "furniture",
+    creator: "Andrew Goode"
+  },
+  {
+    name: "VCR",
+    description: "VCR from 1990's",
+    category: "objects",
+    creator: "Panasonic"
+  },
+  {
+    name: "Gold Coin",
+    description: "An old gold coin",
+    category: "silver",
+    creator: "Augustus Caesar"
+  },
+  {
+    name: "Colorful Purse",
+    description: "A colorful purse made out of cloth",
+    category: "purses",
+    creator: "Prada"
+  },
+  {
+    name: "Knife",
+    description: "A knife made out of iron",
+    category: "objects",
+    creator: "Unknown"
+  },
+  {
+    name: "Armoire",
+    description: "A large wooden armoire",
+    category: "furniture",
+    creator: "Unknown"
+  }
+])
